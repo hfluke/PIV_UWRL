45e5da8f9b3929ddf2c71bfcc159f02127ac6d00
hfluke <h.fluckiger@usu.edu>
2024-06-04 18:25:56 -0600

    run on two puters

--- Batch_PIV.py
+++ Batch_PIV.py
@@ -10,6 +10,9 @@ def batch():
     with open('analyzed_videos.txt') as f:
         for line in f:
             analyzed.append(line.strip())
+    with open('hfluke.txt') as f:
+        for line in f:
+            analyzed.append(line.strip())
 
     videos = []
     for month in months:
@@ -17,29 +20,17 @@ def batch():
             video not in analyzed and videos.append(video)
 
     vid_length = len(videos)
+    i = 0
+    for video in videos:
+        i += 1
+        print(f"Analyzing video {i} of {vid_length}")
+        print(f"{video}")
 
-    waterlab = videos[0:(vid_length//2)]
-    hfluke = videos[(vid_length//2):vid_length]
-
-    for video in waterlab:
-        with open('waterlab.txt', 'a') as f:
-            f.write(f'{video}\n')
-
-    for video in hfluke:
-        with open('hfluke.txt', 'a') as f:
-            f.write(f'{video}\n')
-
-    # i = 0
-    # for video in videos:
-    #     i += 1
-    #     print(f"Analyzing video {i} of {vid_length}")
-    #     print(f"{video}")
-
-    #     PyCam_Implementation(video)
+        PyCam_Implementation(video)
         
-    #     with open('analyzed_videos.txt', "a") as f:
-    #         f.write(f"{video}\n")
+        with open('analyzed_videos.txt', "a") as f:
+            f.write(f"{video}\n")
 
-    #     print()
+        print()
 
 batch()
