ef4810dcdfa7540d0a3b18c3b6a8255df927203f
hfluke <h.fluckiger@usu.edu>
2024-06-10 16:01:04 -0600

    monday eod

--- all_vecs.py
+++ all_vecs.py
@@ -3,10 +3,13 @@ import xarray as xr
 import pandas as pd
 
 vector_files = [f for f in glob.glob('nc_new/*.nc')]
-df = xr.open_dataset(vector_files.pop()).to_dataframe().reset_index()
-i=0
-for f in vector_files:
-    i+=1; print(i)
-    temp = xr.open_dataset(f).to_dataframe().reset_index()
-    df = pd.concat([df, temp])
-df.to_csv('all_vecs.csv', index=False)
+
+n = 0
+while vector_files != []:
+    print(n)
+    dfs = []
+    for i in range(8):
+        dfs.append(xr.open_dataset(vector_files.pop()).to_dataframe().reset_index())
+    pd.concat(dfs).to_csv(f'temp2/temp_{n}.csv', index=False)
+    n += 1
+
