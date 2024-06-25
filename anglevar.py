b393ddd8e33d4f3081407b2b20c445760b1c460f
hfluke <h.fluckiger@usu.edu>
2024-06-25 17:16:56 -0600

    did i make any progress today?

--- anglevar.py
+++ anglevar.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import numpy as np
 
-REPS = 100
+REPS = 110
 
 POINTS = [
-    {'x': -0.13, 'y': -0.13},
-    {'x': -0.13, 'y': 0},
-    {'x': -0.13, 'y': 0.13},
-    {'x': 0, 'y': -0.13},
-    {'x': 0, 'y': 0.13},
-    {'x': 0.13, 'y': -0.13},
-    {'x': 0.13, 'y': 0},
-    {'x': 0.13, 'y': 0.13},
+    {'x': -1, 'y': -1},
+    {'x': -1, 'y': 0},
+    {'x': -1, 'y': 1},
+    {'x': 0, 'y': -1},
+    {'x': 0, 'y': 1},
+    {'x': 1, 'y': -1},
+    {'x': 1, 'y': 0},
+    {'x': 1, 'y': 1},
 ]
 
 analyzed = []
@@ -19,12 +19,15 @@ with open('anglevar_complete.txt') as f:
     for line in f:
         analyzed.append(pd.to_datetime(line))
 
-df = pd.read_csv('temp_UWRL_river_velocimetry_dataset.csv')
+df = pd.read_csv('temp2_UWRL_river_velocimetry_dataset.csv')
 df.datetime = pd.to_datetime(df.datetime)
 
 dates = []
 for date in sorted(df.datetime.unique()):
     date not in analyzed and dates.append(date)
+
+x_vals = sorted(df.x.unique())
+y_vals = sorted(df.y.unique())
 
 while dates != []:
     print(f'{len(dates)} more timestamps to process')
@@ -32,17 +35,22 @@ while dates != []:
 
     for i in range(REPS):
         date = dates.pop(0)
-        print(f'{REPS-i:>3}. {date}')
+        print(f'{REPS-i:>7}. {date}')
 
-        df_curr = df[(df.datetime == date) & ~(df.angle.isna())]
+        df_i = df[(df.datetime == date) & ~(df.angle.isna())]
 
-        for index, row in df_curr.iterrows():
-            xr, yr = round(row.x, 3), round(row.y, 3)
+        for index, row in df_i.iterrows():
+            x, y = row.x, row.y
+            x_i, y_i = x_vals.index(x), y_vals.index(y)
+
             angles = []
 
             for POINT in POINTS:
-                angles.extend(df_curr.loc[(df_curr.x == xr + POINT['x'])&(df_curr.y == yr + POINT['y'])].angle.values)
-            
+                try:
+                    angles.extend(df_i.loc[(df_i.x == x_vals[x_i + POINT['x']]) & (df_i.y == y_vals[y_i + POINT['y']])].angle.values)
+                except:
+                    continue
+
             if angles != []:
                 angle_diffs = []
                 for angle in angles:
@@ -50,10 +58,12 @@ while dates != []:
                     angle_diffs.append(diff * diff)
         
                 df.at[index, 'angle_var'] = sum(angle_diffs) / len(angle_diffs)
+                df.at[index, 'angle_var_support'] = len(angles)
+
         d.append(date)
 
     print('saving to csv\n')
-    df.to_csv('temp_UWRL_river_velocimetry_dataset.csv', index=False)
+    df.to_csv('temp2_UWRL_river_velocimetry_dataset.csv', index=False)
 
     with open('anglevar_complete.txt', 'a') as f:
         for date in d:
