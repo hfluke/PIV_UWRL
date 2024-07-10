bc05e69884cf2e5cff5833c43346f7ff12d375b9
hfluke <h.fluckiger@usu.edu>
2024-07-10 17:37:42 -0600

    reorganizing; restricting AOI for faster video processing

--- Batch_PIV.py
+++ Batch_PIV.py
@@ -1,32 +1,41 @@
 import glob
-from PyCam_Implementation import PyCam_Implementation
+from PyCam_Implementation import PyCam_Implementation, PyCam_Implementation2
+from datetime import datetime
+# import pandas as pd
+
+MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    # 'August',
+    # 'September',
+    # 'October',
+    # 'November',
+    # 'December'
+]
+
 
 def batch():
 
-    MONTHS = [
-        # 'January',
-        # 'February',
-        # 'March',
-        # 'April',
-        # 'May',
-        # 'June',
-        'July',
-        # 'August',
-        # 'September',
-        # 'October',
-        # 'November',
-        # 'December'
-    ]
-
     analyzed = []
-    with open('analyzed_videos.txt') as f:
+    # with open('analyzed_videos.txt') as f:
+    with open('analyzed_videos2.txt') as f:
         for line in f:
             analyzed.append(line.strip())
 
     videos = []
     for month in MONTHS:
         for video in sorted(glob.glob("*.mp4", root_dir=f"{month}/videos/")):
-            video not in analyzed and videos.append(video)
+            # video not in analyzed and videos.append(video)
+            videos.append(video)
+    
+    videos = videos[::37][len(analyzed):]
+
+    # return
 
     vid_length = len(videos)
     i = 0
@@ -35,11 +44,33 @@ def batch():
         print(f"Video {i} of {vid_length}")
         print(f"{video}")
 
-        PyCam_Implementation(video)
+        time_start = datetime.now()
 
-        with open('analyzed_videos.txt', "a") as f:
+        original_area = PyCam_Implementation(video)
+        time_mid = datetime.now()
+        elapsed_original = time_mid - time_start
+        print('Elapsed: ', elapsed_original)
+
+        new_area = PyCam_Implementation2(video)
+        time_end = datetime.now()
+        elapsed_bounded = time_end - time_mid
+        print('Elapsed: ', elapsed_bounded)
+
+        # with open('analyzed_videos.txt', "a") as f:
+        with open('analyzed_videos2.txt', "a") as f:
             f.write(f"{video}\n")
 
+        print(f'original AOI area: {original_area}')
+        print(f'original AOI time: {elapsed_original}')
+        print(f'new AOI area: {new_area}')
+        print(f'new AOI time: {elapsed_bounded}')
+        print(f'original to new area ratio: {original_area/new_area}')
+        print(f'original to new time ratio: {elapsed_original/elapsed_bounded}')
+
+        with open('AOI_analysis.csv', "a") as f:
+            f.write(f"{video},{original_area},{elapsed_original},{new_area},{elapsed_bounded}\n")
+
         print()
+
 
 batch()
--- PyCam_Implementation.py
+++ PyCam_Implementation.py
@@ -5,7 +5,11 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
 from dask.diagnostics import ProgressBar
 
-MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+MONTHS = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+]
+
 
 def PyCam_Implementation(video_file):
 
@@ -13,7 +17,7 @@ def PyCam_Implementation(video_file):
     camera = "cam-config-UWRL.json"
     name = video_file.split(".")[0]
     video_file = f"{month}/videos/{video_file}"
-    vector_file = f"{month}/results/{name}_velocimetry_results.nc"
+    vector_file = f"{month}/results/{name}_velocimetry_results2.nc"
 
     stabilize = [
         [2559, 1919],
@@ -21,10 +25,12 @@ def PyCam_Implementation(video_file):
         [940, 680],
         [200, 1919]
     ]
-    
+
+    cam_config = pyorc.load_camera_config(camera)
+
     video = pyorc.Video(
         video_file,
-        camera_config=pyorc.load_camera_config(camera),
+        camera_config=cam_config,
         start_frame=0,
         end_frame=125,
         stabilize=stabilize,
@@ -65,7 +71,131 @@ def PyCam_Implementation(video_file):
         add_colorbar=True
     )
     plt.title(f"{name} velocimetry results")
-    plt.savefig(f"{month}/results/{name}_velocimetry_results.png", bbox_inches="tight", dpi=600)
+    plt.savefig(f"{month}/results/{name}_velocimetry_results2.png", bbox_inches="tight", dpi=600)
     plt.close() # thank you Haley
 
     ds.close()
+
+    return cam_config.bbox.area
+
+
+def PyCam_Implementation2(video_file):
+
+    month = MONTHS[int(video_file.split('-')[1]) - 1]
+    camera = "cam-config-UWRL.json"
+    name = video_file.split(".")[0]
+    video_file = f"{month}/videos/{video_file}"
+    vector_file = f"{month}/results/{name}_velocimetry_results_bounded.nc"
+
+    # this is to check if processing videos with smaller aoi area is faster/better results
+    cam_config = pyorc.load_camera_config(camera)
+    cam_config.set_bbox_from_corners(corners(month))
+
+    # return
+
+    stabilize = [
+        [2559, 1919],
+        [1752, 680],
+        [940, 680],
+        [200, 1919]
+    ]
+    
+    video = pyorc.Video(
+        video_file,
+        camera_config=cam_config,
+        start_frame=0,
+        end_frame=125,
+        stabilize=stabilize,
+        h_a=0. # water level must be of type float
+    )
+
+    da = video.get_frames()
+    da = da.frames.normalize()
+    da = da.frames.project()
+    piv = da.frames.get_piv().to_netcdf(vector_file, compute=False)
+    with ProgressBar():
+        piv.compute()
+
+    ds = xr.open_dataset(vector_file)
+
+    ds.velocimetry.mask.corr(inplace=True)
+    ds.velocimetry.mask.minmax(inplace=True)
+    ds.velocimetry.mask.rolling(inplace=True)
+    ds.velocimetry.mask.outliers(inplace=True)
+    ds.velocimetry.mask.variance(inplace=True)
+    ds.velocimetry.mask.angle(angle_tolerance=0.5*np.pi)
+    ds.velocimetry.mask.count(inplace=True)
+    ds.velocimetry.mask.window_mean(wdw=2, inplace=True, tolerance=0.5, reduce_time=True)
+
+    video = pyorc.Video(video_file, start_frame=0, end_frame=125)
+    video.camera_config = ds.velocimetry.camera_config
+    
+    ds = ds.mean(dim="time", keep_attrs=True)
+
+    ds.velocimetry.plot(
+        ax=video.get_frames(method="rgb")[0].frames.plot(mode="camera").axes,
+        mode="camera",
+        alpha=0.4,
+        cmap="rainbow",
+        scale=200,
+        width=0.0015,
+        norm=Normalize(vmin=0., vmax=1.0, clip=False),
+        add_colorbar=True
+    )
+    plt.title(f"{name} velocimetry results")
+    plt.savefig(f"{month}/results/{name}_velocimetry_results_bounded.png", bbox_inches="tight", dpi=600)
+    plt.close() # thank you Haley
+
+    ds.close()
+
+    return cam_config.bbox.area
+
+
+def corners(month):
+    if month in ['January', 'February', 'March']:
+        corners = [
+            [2375, 1100],
+            [1752, 680],
+            [940, 680],
+            [475, 1100]
+        ]
+    elif month == 'April':
+        corners = [
+            [2475, 1125],
+            [1752, 680],
+            [940, 680],
+            [475, 1125]
+        ]
+    elif month == 'May':
+        # default; May vectors inconsistent
+        corners = [
+            [2559, 1919],
+            [1752, 680],
+            [940, 680],
+            [200, 1919]
+        ]
+    elif month == 'June':
+        corners = [
+            [2550, 1750],
+            [2550, 1250],
+            [350, 1250],
+            [0,1750]
+        ]
+    elif month == 'July':
+        # default; July vectors inconsistent
+        corners = [
+            [2559, 1919],
+            [1752, 680],
+            [940, 680],
+            [200, 1919]
+        ]
+    else:
+        # default; July vectors inconsistent
+        corners = [
+            [2559, 1919],
+            [1752, 680],
+            [940, 680],
+            [200, 1919]
+        ]
+    
+    return corners
