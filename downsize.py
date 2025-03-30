593a79e139e2d3cd181a2ab871c0b57e2fa7bcbf
Hannah Fluckiger <h.fluckiger@usu.edu>
2025-03-30 14:32:33 -0600

    running all analysis notebooks

---
+++ downsize.py
@@ -0,0 +1,33 @@
+N = 9_000_000 # 12_603_976
+
+# with open("UWRL_river_velocimetry_dataset.csv", "r") as f:
+#     file = f.readlines()
+
+# # print(len(file))
+# print("X")
+# i = 0
+# with open("UWRL_river_velocimetry_dataset_NEW.csv", "w") as f:
+#     for line in file:
+#         f.write(line)
+            
+#         i += 1
+#         if i > N:
+#             break   
+
+print("X")
+with open("UWRL_river_velocimetry_dataset_angle_variance.csv", "r") as f:
+    file = f.readlines()
+
+# print(len(file))
+print("X")
+
+i = 0
+with open("UWRL_river_velocimetry_dataset_angle_variance_NEW.csv", "w") as f:
+    for line in file:
+        f.write(line)
+            
+        i += 1
+        if i > N:
+            break   
+
+print("X")
